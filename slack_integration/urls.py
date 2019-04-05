8cbe4e35c89720c9205c4427141b4edc76e348f0
cmavelis <cmavelis@gmail.com>
2019-04-05 12:08:25 -0400

    added an event creation object

--- slack_integration/urls.py
+++ slack_integration/urls.py
@@ -6,4 +6,7 @@ app_name = 'slack_integration'
 urlpatterns = [
     # slack/test/  slack test message
     path('test/', views.slack_test, name='slack_test'),
+
+    # slack/create_event/  create an event from slack
+    path('create_event/', views.slack_create_event, name='slack_create_event')
 ]
--- slack_integration/views.py
+++ slack_integration/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 
+from team.models import Event
+
 
 @csrf_exempt
 def slack_test(request):
@@ -16,6 +18,29 @@ def slack_test(request):
             'attachments': [
                 {
                     'text': 'Sent by {}'.format(payload['user_name'])
+                }
+            ]
+        }
+
+        return JsonResponse(response)
+
+
+@csrf_exempt
+def slack_create_event(request):
+    if request.method == 'POST':
+        payload = request.POST
+    else:
+        return Http404
+    print(payload)
+    if payload['command'] == '/create_event':
+        event_name = payload['text']
+        Event.objects.create(name=event_name, type='T')
+
+        response = {
+            'text': 'Your event has been created',
+            'attachments': [
+                {
+                    'text': 'Event name: {}'.format(event_name)
                 }
             ]
         }

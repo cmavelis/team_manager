27212b1ca75c4c36efbf0d060c8d57dfab092f27
cmavelis <cmavelis@gmail.com>
2019-04-05 16:10:19 -0400

    adding Other option to events

--- team/models.py
+++ team/models.py
@@ -14,6 +14,7 @@ class Event(models.Model):
     EVENT_TYPES = (
         ('T', 'Tournament',),
         ('S', 'Scrimmage',),
+        ('O', 'Other',),
     )
 
     type = models.CharField(max_length=20, choices=EVENT_TYPES)

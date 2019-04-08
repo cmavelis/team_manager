1d7abe93851d05bbbf084f0aa15f1e00e7cdfc4e
cmavelis <cmavelis@gmail.com>
2019-04-08 09:35:41 -0400

    trying a different way

--- slack_integration/views.py
+++ slack_integration/views.py
@@ -49,7 +49,72 @@ def slack_create_event(request):
 
         interactive_response = {
             'text': 'Supply some more information',
-            'attachments': create_event
+            'attachments': [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Please enter some details for the event."
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Event date:"
+            },
+            "accessory": {
+                "type": "datepicker",
+                "initial_date": "2019-04-28",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select a date",
+                    "emoji": 'true'
+                }
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Event type:"
+            },
+            "accessory": {
+                "type": "static_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select an item",
+                    "emoji": 'true'
+                },
+                "options": [
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Tournament",
+                            "emoji": 'true'
+                        },
+                        "value": "T"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Scrimmage",
+                            "emoji": 'true'
+                        },
+                        "value": "S"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Other",
+                            "emoji": 'true'
+                        },
+                        "value": "O"
+                    }
+                ]
+            }
+        }
+    ]
         }
 
         return JsonResponse(interactive_response, safe=False)

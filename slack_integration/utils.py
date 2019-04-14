181f6c5e855960ca7cf68dff51e906fb517e90ae
Cameron Avelis <cmavelis@gmail.com>
2019-04-14 16:49:35 -0400

    trying block message

--- slack_integration/utils.py
+++ slack_integration/utils.py
@@ -9,8 +9,30 @@ def send_slack_event_confirm(event, player):
     message = {
         "token": os.environ["SLACK_TOKEN"],
         "channel": player.slack_user_id,
-        "as_user": False,
-        "text": "test"
+        "as_user": True,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "We don't have a response from you for *%s*-- can you make it?" % event.name
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": str(allowed_response),
+                            "emoji": True
+                        },
+                        "value": str(allowed_response)
+                    } for allowed_response in Attendance.ATTENDANCE_TYPES
+                ]
+            }
+        ]
     }
 
     return message

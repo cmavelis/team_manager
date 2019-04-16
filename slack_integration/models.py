a1809738d04000b2190741b03e3379108543d856
cmavelis <cmavelis@gmail.com>
2019-04-16 17:16:19 -0400

    models

--- slack_integration/models.py
+++ slack_integration/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-from team.models import Player, Event
-
 
 class InteractiveMessage(models.Model):
     slack_message_ts = models.CharField(max_length=20)
-    player_id = models.IntegerField(blank=True)
-    event_id = models.IntegerField(blank=True)
+    player_id = models.IntegerField(blank=True, null=True)
+    event_id = models.IntegerField(blank=True, null=True)

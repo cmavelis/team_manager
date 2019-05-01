c9d890ddb38e2f24130fddcc2f739c0db0b44dfe
cmavelis <cmavelis@gmail.com>
2019-05-01 16:11:46 -0400

    adding gmail account for sending recovery email

--- team_manager/settings.py
+++ team_manager/settings.py
@@ -184,6 +184,13 @@ STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
+# Gmail email config TODO: use sendGrid
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('GMAIL_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
 
 
 SITE_ID = 1

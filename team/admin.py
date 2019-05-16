80f2aaa1244778cdcddb9439f09a5d371701919d
cmavelis <cmavelis@gmail.com>
2019-05-16 14:21:00 -0400

    editing event list

--- team/admin.py
+++ team/admin.py
@@ -22,7 +22,7 @@ class PlayerAdmin(admin.ModelAdmin):
 
 class EventAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,                  {'fields': ['name', 'id']}),
+        (None,                  {'fields': ['name']}),
         ('Event Details',          {'fields': ['type', 'date', ]}),
     ]
 

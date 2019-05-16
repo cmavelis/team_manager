a9540c87d02109ffd0d9d6450fceafc2511fbfcb
cmavelis <cmavelis@gmail.com>
2019-05-16 13:59:42 -0400

    removing fstrings

--- slack_integration/utils.py
+++ slack_integration/utils.py
@@ -17,7 +17,7 @@ def compose_message(channel, **kwargs):
 
 
 def send_slack_event_confirm(event, player, msg_id):
-    print(f"{event = }", f"{player = }")
+    print(event, player)
     question_block = {
         "type": "section",
         "text": {

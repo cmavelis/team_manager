df839a9ebb97d0b30a02df54ebdc02bb3e3aa2dd
Cameron Avelis <cmavelis@gmail.com>
2019-06-15 23:02:06 -0400

    added entry to admin

--- team/admin.py
+++ team/admin.py
@@ -23,7 +23,7 @@ class PlayerAdmin(admin.ModelAdmin):
 class EventAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields': ['name']}),
-        ('Event Details',          {'fields': ['type', 'date', ]}),
+        ('Event Details',          {'fields': ['type', 'date', 'display_event']}),
     ]
 
 
